950f5f8bb37bbef671cdab77042fd5b81f799653
xenonoize <pim.vanenk@gmail.com>
2015-11-25 15:06:20 +0100

    Added functions for deleting and editing menu-items

---
+++ Admin/Index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <a href="MenuItem/">
+            Menu-items
+        </a>
+    </body>
+</html>
