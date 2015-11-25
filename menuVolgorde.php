523de165248a2ff88b1b8bd3a739e000c2f57a57
eriknugteren <erik.nugteren@ziggo.nl>
2015-11-25 10:54:46 +0100

    info pagina voor restaurant + stylsheet

---
+++ menuVolgorde.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+Erik Nugteren
+S1090245
+ICTm1i
+2015 -->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>formulierNaamVolgorde</title>
+    </head>
+    <body>
+        <form>
+            Naam: <input type="text" name="Naam"><br>
+            Nummer: <input type="text" name="Nummer"><br>
+        </form>
+    </body>
+</html>
