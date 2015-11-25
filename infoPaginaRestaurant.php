07f5cd1ecd3da38911acc7568a8c449b7f1a4352
eriknugteren <erik.nugteren@ziggo.nl>
2015-11-25 10:43:17 +0100

    info pagina voor restaurant

---
+++ infoPaginaRestaurant.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+Erik Nugteren
+S1090245
+ICTm1i
+2015 -->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>infopagina</title>
+        <link rel="stylesheet" href="css/stijlInfoPaginaRestaurant.css">
+    </head>
+    <body>
+        <div id="header">
+            De Viersprong
+        </div>
+        <div id="menu">
+            <ul>
+                <li>Home</li>
+                <li>Agenda</li>
+                <li>Reserveren</li>
+                <li>Hagel en Donder</li>
+                <li>Recensies</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        <div id="main">
+            <div id="links">
+                linkergedeelte<br><br><br><br><br><br>
+            </div>
+            <div id="rechts">
+                rechtergedeelte<br><br><br><br><br><br>
+            </div>
+        </div>
+        <div id="copyright">
+            © de Viersprong 2015
+        </div>
+    </body>
+</html>
